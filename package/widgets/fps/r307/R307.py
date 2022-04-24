789c76dffba8ff60c17ca9a92baed3420eac38ca
Sarthak Jain <sarthakjain1337@gmail.com>
2022-04-24 12:28:42 +0530

    Added functions for scanning and enrolling users

--- package/widgets/fps/r307/R307.py
+++ package/widgets/fps/r307/R307.py
@@ -1,6 +1,7 @@
 '''
     R307.py
-        - Contains controller class for r307 fingerprint sensor
+    
+    - Contains controller class for r307 fingerprint sensor
 
     [NOTE]:
     Only configured for Windows (COM3) and Linux (/dev/ttyUSB with root privileges)
@@ -8,11 +9,13 @@
 
 from sys import platform
 import logging
+import time
+import hashlib
 
 # from pyfingerprint import pyfingerprint
 from pyfingerprint.pyfingerprint import PyFingerprint
-# from pyfingerprint.pyfingerprint import FINGERPRINT_CHARBUFFER1
-# from pyfingerprint.pyfingerprint import FINGERPRINT_CHARBUFFER2
+from pyfingerprint.pyfingerprint import FINGERPRINT_CHARBUFFER1
+from pyfingerprint.pyfingerprint import FINGERPRINT_CHARBUFFER2
 
 
 class R307():
@@ -43,9 +46,66 @@ class R307():
         self.logger.info(f"Setting port to {self.port}")
 
     def enroll_fingerprint(self):
-        ''' Enroll a new fingerprint '''
-        pass
+        '''
+            - Enroll a new fingerprint
+            - Keeps running until a finger enrolled (2 successful scans)
+            - Returns True if both fingers matched, otherwise returns False
+        '''
+
+        # Scan 1
+        self.logger.debug("Enrolling new user")
+        while(self.dev.readImage() == False):
+            continue
+
+        # Save new scan characteristics to buffer
+        self.dev.convertImage(FINGERPRINT_CHARBUFFER1)
+        self.logger.debug("First scan complete")
+
+        time.sleep(2)
+
+        # Scan 2
+        while(self.dev.readImage() == False):
+            continue
+
+        # Save new scan characteristics to buffer
+        self.dev.convertImage(FINGERPRINT_CHARBUFFER2)
+        self.logger.debug("Second scan complete")
+
+        if self.dev.compareCharacteristics() == 0:
+            # Fingers do not match, we cannot add this template to database
+            return False
+
+        # Add this template to database
+        self.dev.createTemplate()
+        position = self.dev.storeTemplate()
+
+        return True
 
     def scan(self):
-        ''' Keep reading fingerprint until a match is found '''
-        pass
+        '''
+            - Read a fingerprint and tries to match it with an existing template in the database
+            - If a match is found, returns the hash of the characteristics of matching template
+        '''
+
+        while(self.dev.readImage() == False):
+            continue
+
+        self.dev.convertImage(FINGERPRINT_CHARBUFFER1)
+        position, accuracy = self.dev.searchTemplate()
+
+        if position == -1 or accuracy < 0.5:
+            return False, None
+
+        # Load characteristics in buffer and compute hash
+        self.dev.loadTemplate(position, FINGERPRINT_CHARBUFFER1)
+        characteristics = self.dev.downloadCharacteristics(FINGERPRINT_CHARBUFFER1)
+        characteristics = str(characteristics).encode('utf-8')
+        characteristics_hash = hashlib.sha256(characteristics).hexdigest()
+
+        return True, characteristics_hash
+
+    # Test only
+    def clear_database(self):
+        ''' Clears all saved fingerprint '''
+        self.logger.info("Clearing fingerprint database.")
+        self.dev.clearDatabase()
--- package/widgets/test.py
+++ package/widgets/test.py
@@ -11,7 +11,7 @@ from fps import FingerprintSensor
 if __name__ == "__main__":
 
     # Setup logging
-    
+
     logfile = f"widgets-test-session.log"
 
     logging.basicConfig(
@@ -23,5 +23,27 @@ if __name__ == "__main__":
     )
 
     # [r307] Basic Tests
+
     ## Test 1: Make sure we can access the sensor
     d = FingerprintSensor()
+
+    ## Test 2: Enroll a new user
+    enrolled = d.enroll_fingerprint()
+    if enrolled == False:
+        print("[-] Failed to enroll new user")
+    else:
+        print("[+] Successfully enrolled new user")
+
+    ## Test 3: Test if we can recognize the newly enrolled user
+    found, characteristics_hash = d.scan()
+    if found == False:
+        print("[-] Enrolled user was not found!")
+    else:
+        print(f"[+] Found user (SHA256:{characteristics_hash})")
+
+    # Clear database after tests
+    q = input("Do you want to clear the database?\n(y/n):")
+    if q.lower() == 'y':
+        d.clear_database()
+
+    print("[+] Completed all tests")
